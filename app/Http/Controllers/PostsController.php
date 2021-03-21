0a41e9366a0bafd8eea47f545e037f1de692e4d6
Sebastian Kut <sebastian_kut@yahoo.co.uk>
2021-03-21 21:33:39 +0100

    initial commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+// class PostsController extends Controller
+// {
+//     public function show($slug)
+//     {
+//         // $posts = [
+//         //     'first' => 'This is first post',
+//         //     'second' => 'This is second post'
+//         // ];
+
+//         // if (!array_key_exists($slug, $posts)) abort(404, "The post doesn't exist");
+
+//         // return view('posts', [
+//         //     'post' => $posts[$slug]
+//         // ]);
+//     }
+// }
+
+
+class PostsController extends Controller
+{
+    public function show($slug)
+    {
+        //Get data from mysql database using queries
+
+        //  $post = DB::table('posts')->where('slug', $slug)->first();
+
+        //  if (!$post) abort(404);
+
+
+        //OR
+        //Using Eloquent and Model
+
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        return view('posts', [
+            'post' => $post
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function () {
+    //get value of a parameter called 'name' from url request and assign to variable $somevariable
+    $somevariable = request('name');
+
+    //return view called 'test' and pass variable called 'name' that = to $somevariable
+    //then in blade view we can return 'name'    
+    return view('test', [
+        'name' => $somevariable
+        //or
+        //'name' => request('name')
+    ]);
+});
+
+
+//using wildcards {post}
+// Route::get('/posts/{post}', function ($post) {
+
+//     //create some mock data 
+//     $posts = [
+//         'first' => 'This is first post',
+//         'second' => 'This is second post'
+//     ];
+
+//     //if value of wildcard {post} doesnt exist in an array $posts return 404
+
+//     if (!array_key_exists($post, $posts)) abort(404, "The post doesn't exist");
+
+
+//     return view('posts', [
+//         'post' => $posts[$post]
+//     ]);
+// });
+
+
+//Proper way of doing the above using CONTROLER
+//we put the above callback function inside PostsController method called show
+Route::get('/posts/{post}', 'App\Http\Controllers\PostsController@show');
